c4aaf128b4000eba8d75bb33bc3e365340084291
Cfairbanx <69484576+Cfairbanx@users.noreply.github.com>
2020-11-03 09:36:18 -0800

    v3

--- techproject/src/mainapp/mainapp/urls.py
+++ techproject/src/mainapp/mainapp/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 
@@ -23,6 +24,7 @@ from .views import home
 urlpatterns = [
     path('', views.home, name='home'),
     path('admin/', admin.site.urls),
+    path('', include('products.urls')),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
